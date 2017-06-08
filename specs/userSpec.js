f89f3dee8a87cfa8f77957b3faea4f9b2c8e36f5
Mariela Cruz <mariela.cruz@jalasoft.com>
2017-06-08 00:31:57 -0400

    addchanges in userspec

--- specs/userSpec.js
+++ specs/userSpec.js
@@ -2,7 +2,7 @@ var request = require('superagent');
 
 describe('User service', function() {
 
-	it('should send a verification e-mail after creating a new user', function(done){
+	xit('should send a verification e-mail after creating a new user', function(done){
 
 	    /*  	
 	    steps:
@@ -15,16 +15,62 @@ describe('User service', function() {
 		    verify that e-mail subject is "Welcome to Todo.ly!"
 		    verify that todoly e-mail contains "Your account has been created"
 	    */
-        
-        request
-            .get('https://www.mailinator.com/fetch_inbox?x=2&to=sk-test&zone=public')
+		//////////
+		request
+		.post('https://todo.ly/api/user.json')
+		.send({
+				Email: 'sk-test000@mailinator.com',
+				FullName: 'myfullname',
+				Password:'pass'
+			})
+		.set('Accept', 'application/json')
+		.end(function(err, res){
+			if(err){
+				return done(err);
+			}
+			//console.log(err);
+			//console.log(res);
+			
+			if(inbox.messages[i].subject == todolySubject){
+			emailId = inbox.messages[i].id;
+			console.log('----------');
+			console.log(emailId);
+			var inboxListRequest = 'https://www.mailinator.com/fetch_inbox?x=2&zone=public&to='+'emailId;
+			
+			request
+            .get(inboxListRequest)
             .end(function(err, res){
                 if (err) {
                     return done(err);
                 }
                 var inbox = res.body;
+				console.log(inbox);
+				var todolySubject = 'Welcome to Todo.ly!';
+				var isTodolyEmailAvailable = 0;
+				var emailId = null;
+		
+			}
+		
+		//////////
+        
+        request
+            .get(inboxListRequest)
+            .end(function(err, res){
+                if (err) {
+                    return done(err);
+                }
+                var inbox = res.body;
+				console.log(inbox);
+				var todolySubject = 'Welcome to Todo.ly!';
+				var isTodolyEmailAvailable = 0;
+				var emailId = null;
+							//	console.log(todolySubject);
+
+				
                 for(var i = 0; i < inbox.messages.length; i++){
-                    console.log(inbox.messages[i].subject);
+                  
+					console.log(inbox.messages[i].subject);
+					//console.log(inbox.messages[i].id);
 
                 }
 
@@ -32,6 +78,86 @@ describe('User service', function() {
 
             });
 
+	});
+	
+	xit('should get todoly e-mail', function(done){
+		request
+		.get('https://www.mailinator.com/fetch_email?msgid=1496867094-20005454684-sk-test00&zone=public')
+		.end(function(err, res){
+			if(err){
+				return done(err);
+			}
+			var email = res.body;
+			console.log('emailllllllllllll');
+			console.log(email);
+			console.log('============== subject    ====');
+			console.log(email.data.subject);
+			console.log('============== parts   ====');
+			console.log(email.data.parts[0].body);
+			done();
+		});
+		
+	});
+	
+	it('should create a new user', function(done){
+		request
+		.post('https://todo.ly/api/user.json')
+		.send({
+				Email: 'sk-test03@mailinator.com',
+				FullName: 'myfullname',
+				Password:'pass'
+			})
+		.set('Accept', 'application/json')
+		.end(function(err, res){
+			if(err){
+				return done(err);
+			}
+			//console.log(err);
+			console.log(res);
+			
+			if(inbox.messages[i].subject == todolySubject){
+			emailId = inbox.messages[i].id;
+			console.log('----------');
+			console.log(emailId);
+			}
+			
+	xit('should send a verification e-mail after creating a new user', function(done){
+
+	         
+        request
+            .get('https://www.mailinator.com/fetch_inbox?x=2&to=sk-test00&zone=public')
+            .end(function(err, res){
+                if (err) {
+                    return done(err);
+                }
+                var inbox = res.body;
+				console.log(inbox);
+				var todolySubject = 'Welcome to Todo.ly!';
+				var isTodolyEmailAvailable = 0;
+				var emailId = null;
+							//	console.log(todolySubject);
+
+				
+                for(var i = 0; i < inbox.messages.length; i++){
+                  
+					console.log(inbox.messages[i].subject);
+					//console.log(inbox.messages[i].id);
+
+                }
+
+                done();
+
+            });
+
+	});
+			
+			
+			}
+
+			
+			
+			done();
+		});
 	});
 });
 
